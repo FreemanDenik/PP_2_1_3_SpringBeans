b15d44d67c0a8120b6b86cd28528cc73902021b9
Freeman <hhaid@bk.ru>
2022-05-10 19:13:13 +0300

    little edit

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -7,20 +7,26 @@ import app.model.Timer;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Application {
 
     public static void main(String[] args) {
-        ApplicationContext applicationContext =
-                new AnnotationConfigApplicationContext(AppConfig.class);
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        List<Long> timer = new ArrayList<>();
         for (int i = 0; i < 5; i++) {
-            System.out.println( applicationContext.getBean(Timer.class).getTime() );
-            System.out.println("----");
+
             AnimalsCage bean =
                     applicationContext.getBean(AnimalsCage.class);
             bean.whatAnimalSay();
-
+            timer.add(applicationContext.getBean(Timer.class).getTime());
         }
 
+        boolean result = timer.stream().allMatch(t->t == applicationContext.getBean(Timer.class).getTime());
+        System.out.println("Ответ: " + result);
 
     }
 
