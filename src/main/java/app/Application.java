de7363498f10de917f32b945ded4a8e9879a30e8
Freeman <hhaid@bk.ru>
2022-05-10 19:07:55 +0300

    done!

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -2,6 +2,8 @@ package app;
 
 import app.config.AppConfig;
 import app.model.AnimalsCage;
+import app.model.Dog;
+import app.model.Timer;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
@@ -11,10 +13,15 @@ public class Application {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
         for (int i = 0; i < 5; i++) {
+            System.out.println( applicationContext.getBean(Timer.class).getTime() );
+            System.out.println("----");
             AnimalsCage bean =
                     applicationContext.getBean(AnimalsCage.class);
             bean.whatAnimalSay();
+
         }
+
+
     }
 
 }
---
+++ src/main/java/app/model/Dog.java
@@ -0,0 +1,11 @@
+package app.model;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Dog extends Animal {
+    public String toString(){
+        return "I'm a dog";
+    }
+}
